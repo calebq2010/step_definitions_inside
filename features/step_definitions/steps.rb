e2a5cbd535465347d53ddcc82c7b4ef430864eb9
Caleb Quinn <Caleb.Quinn@teamnorthwoods.com>
2018-12-05 10:51:29 -0500

    Stopped on page 122 for storing state in the world

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -9,6 +9,13 @@ class Account
 
 end
 
+class Teller
+  def withdraw_from(account, amount)
+
+  end
+
+end
+
 #no longer used in Cucumber 3.0.0
 # CAPTURE_A_NUMBER = Transform /^\d+$/ do |number|
 #   number.to_i
@@ -16,21 +23,21 @@ end
 
 #ParameterType has replaced transform
 ParameterType(
-  name: 'CAPTURE_A_NUMBER',
+  name: 'CAPTURE_CASH_AMOUNT',
   regexp: /^$(\d+$)/,
   transformer: -> (number) { number.to_i }
 )
 
 Given /^I have deposited \$(\d+) in my Account$/ do |amount|
-    my_account = Account.new
-    my_account.deposit(amount)
-    my_account.balance.should eq(amount),
+    @my_account = Account.new
+    @my_account.deposit(amount)
+    @my_account.balance.should eq(amount),
       "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
-When /^I request \$(\d+)/ do |request_amount|
-  #pending("Need to design the request interface")
-  puts request_amount
+When /^I withdrawal \$(\d+)/ do |request_amount|
+  teller = Teller.new
+  teller.withdraw_from(@my_account, request_amount)
 end
 
 Then /^\$(\d+) should be deposited/ do |deposited_amount|
--- features/withdrawal.feature
+++ features/withdrawal.feature
@@ -2,5 +2,5 @@ Feature: Cash Withdrawal
 
   Scenario: Successful withdrawal from an account in credit
     Given I have deposited $100 in my Account
-    When I request $20
+    When I withdrawal $20
     Then $20 should be deposited
