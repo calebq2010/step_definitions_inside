e2e9e93b7c34ec50962aa2a339f1f53cbf2cc04d
Caleb Quinn <Caleb.Quinn@teamnorthwoods.com>
2018-12-13 10:25:10 -0500

    removed duplication from account_steps file in the account step

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -1,7 +1,5 @@
 Then /^my account has been credited with \$(\d+)$/ do |amount|
     my_account.credit(amount)
-    my_account.balance.should eq(amount),
-      "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
 Then /^the balance of my account should be \$(\d+)/ do |amount|
